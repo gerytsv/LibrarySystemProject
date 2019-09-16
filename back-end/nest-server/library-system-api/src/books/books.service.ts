80d1e3a5bfbd3f8632521e8f1525c913a9f3d24d
Georgi Krastev <gkrastev9715@abv.bg>
2019-09-16 21:09:49 +0300

    adding test services

--- back-end/nest-server/library-system-api/src/books/books.service.ts
+++ back-end/nest-server/library-system-api/src/books/books.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Book } from '../database/entities/books.entity';
 import { Repository } from 'typeorm';
+import { BookDTO } from './models/book.dto';
+import { Book } from '../database/entities/books.entity';
+import { CreateBookDTO } from './models/create-book.dto';
 
 @Injectable()
 export class BooksService {
     public constructor(
-        @InjectRepository(Book) private readonly booksRepository: Repository<Book>
-      ) {}
+        @InjectRepository(Book) private readonly booksRepository: Repository<BookDTO>
+    ) {}
+
+    public  async allBooks(withDeleted: boolean = false): Promise<BookDTO[]> {
+        return withDeleted
+          ? await this.booksRepository.find()
+          : await this.booksRepository.find({where: { isDeleted : false}}
+          );
+        }
+
+    public async createBook(book: CreateBookDTO): Promise<BookDTO> {
+      return  await this.booksRepository.save(book);
+    }
 }
