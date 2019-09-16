d9e557280433dfb607b0feb39957ec385aa9db47
Gergana Tsvetkova <gerytsv@gmail.com>
2019-09-16 23:01:36 +0300

    added getBooks(?byTitle) functionality

--- back-end/nest-server/library-system-api/src/books/books.controller.ts
+++ back-end/nest-server/library-system-api/src/books/books.controller.ts
@@ -1,13 +1,20 @@
+import { BookDTO } from './models/book.dto';
 import { BooksService } from './books.service';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('books')
 export class BooksController {
   public constructor(private readonly booksDataService: BooksService) {}
 
   @Get()
-  public async getBooks() {
-    const books = await this.booksDataService.allBooks();
+  public async getBooks(@Query('title') title: string): Promise<BookDTO[]> {
+    const books: BookDTO[] = await this.booksDataService.allBooks();
+
+    if (title) {
+      return books.filter(book =>
+        book.title.toLowerCase().includes(title.toLowerCase()),
+      );
+    }
     return books;
   }
   // @Post()
