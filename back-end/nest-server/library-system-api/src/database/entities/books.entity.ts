1608635046afe9e614b8decb399516ad55ddd147
Georgi Krastev <gkrastev9715@abv.bg>
2019-09-16 17:14:40 +0300

    testing entity added

---
+++ back-end/nest-server/library-system-api/src/database/entities/books.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('books')
+export class Todo {
+    @PrimaryGeneratedColumn('uuid')
+    public id: string;
+
+    @Column()
+    public title: string;
+
+    @Column()
+    public author: string;
+
+    @Column()
+    public year: string;
+
+    @Column({type: 'boolean', default: false})
+    public freeToBorrow: boolean;
+
+    @Column({type: 'boolean', default: false})
+    public isDeleted: boolean;
+}
