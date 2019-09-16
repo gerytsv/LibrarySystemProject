302dc5973fd6f44425791c72821c572c6787ec02
Georgi Krastev <gkrastev9715@abv.bg>
2019-09-16 18:25:01 +0300

    adding testing dto

---
+++ back-end/nest-server/library-system-api/src/books/models/book.dto.ts
@@ -0,0 +1,14 @@
+export class BookDTO {
+
+    public id: string;
+
+    public title: string;
+
+    public author: string;
+
+    public year: string;
+
+    public freeToBorrow: boolean;
+
+    public isDeleted: boolean;
+}
