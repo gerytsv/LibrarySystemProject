9a4b8f851b2bd76d9f026b71247392af7ad3bc27
Georgi Krastev <gkrastev9715@abv.bg>
2019-09-18 22:09:12 +0300

    adding seed db file

---
+++ back-end/nest-server/library-system-api/src/database/seed/seed.ts
@@ -0,0 +1,36 @@
+import { createConnection } from 'typeorm';
+import { UserRole } from '../../users/enums/user-roles.enum';
+import { Role } from '../entities/roles.entity';
+import { User } from '../entities/users.entity';
+
+const main = async () => {
+
+  const connection = await createConnection();
+  const roleRepo = connection.getRepository(Role);
+  const userRepo = connection.getRepository(User);
+
+  const admin = roleRepo.create({
+    name: UserRole[UserRole.Admin],
+  });
+  const adminRole = await roleRepo.save(admin);
+  const basic = await roleRepo.create({
+    name: UserRole[UserRole.Basic],
+  });
+  const basicRole = await roleRepo.save(basic);
+
+  const firstAdmin = new User();
+  firstAdmin.username = 'John',
+  firstAdmin.password = 'test';
+  firstAdmin.roles = await Promise.resolve([adminRole, basicRole]);
+
+  userRepo.create(firstAdmin);
+  await userRepo.save(firstAdmin);
+
+  await connection.close();
+
+  console.log(`Data seeded successfully`);
+
+};
+
+main()
+  .catch(console.log);
