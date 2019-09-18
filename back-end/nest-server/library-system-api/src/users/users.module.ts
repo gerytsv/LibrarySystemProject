cf8da2d093f9db2bb6057f56dd7d173ab7c63619
Georgi Krastev <gkrastev9715@abv.bg>
2019-09-18 22:09:35 +0300

    adding users module

---
+++ back-end/nest-server/library-system-api/src/users/users.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { DatabaseModule } from '../database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from '../database/entities/books.entity';
+import { User } from '../database/entities/users.entity';
+import { Role } from '../database/entities/roles.entity';
+
+@Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([User, Role])],
+  providers: [UsersService],
+  controllers: [UsersController]
+})
+export class UsersModule {}
