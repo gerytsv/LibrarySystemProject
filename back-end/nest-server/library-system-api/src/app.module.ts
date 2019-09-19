ee96a99562cb53ffa6e2d9fa7049c742ebcf507f
Georgi Krastev <gkrastev9715@abv.bg>
2019-09-19 19:39:25 +0300

    adding the review module

--- back-end/nest-server/library-system-api/src/app.module.ts
+++ back-end/nest-server/library-system-api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [DatabaseModule, BooksModule, UsersModule],
+  imports: [DatabaseModule, BooksModule, UsersModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
