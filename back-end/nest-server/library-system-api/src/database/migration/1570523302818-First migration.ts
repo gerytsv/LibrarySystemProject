cf872178929e330855d0654848afbd0c61015240
Georgi Krastev <gkrastev9715@abv.bg>
2019-10-08 11:41:28 +0300

    adding initial migration

---
+++ back-end/nest-server/library-system-api/src/database/migration/1570523302818-First migration.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+
+export class FirstMigration1570523302818 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        //
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        //
+    }
+
+}
