62020d784c31af99d8bf8bfdfa03f17c156b48dd
Georgi Krastev <gkrastev9715@abv.bg>
2019-10-08 14:10:37 +0300

    adding authorization

--- back-end/nest-server/library-system-api/src/ratings/ratings.module.ts
+++ back-end/nest-server/library-system-api/src/ratings/ratings.module.ts
@@ -5,9 +5,10 @@ import { Rating } from '../database/entities/ratings.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from '../database/entities/books.entity';
 import { User } from '../database/entities/users.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Rating, Book, User])],
+  imports: [TypeOrmModule.forFeature([Rating, Book, User]), AuthModule],
   providers: [RatingsService],
   controllers: [RatingsController],
 })
