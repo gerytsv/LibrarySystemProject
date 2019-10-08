3d9d38cf91255f3febe6948b30483b0d3e527737
Georgi Krastev <gkrastev9715@abv.bg>
2019-10-08 14:00:21 +0300

    some minor changes

--- back-end/nest-server/library-system-api/src/books/models/create-book.dto.ts
+++ back-end/nest-server/library-system-api/src/books/models/create-book.dto.ts
@@ -1,5 +1,10 @@
+import { Publish } from '../../transformer/decorators/publish';
+
 export class CreateBookDTO {
+    @Publish()
     public title: string;
+    @Publish()
     public author: string;
+    @Publish()
     public year: string;
   }
