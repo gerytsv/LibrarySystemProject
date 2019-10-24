a88e20deb59bfa8b6db04caefd79d3466b6acaa0
Georgi Krastev <gkrastev9715@abv.bg>
2019-10-24 12:04:52 +0300

    starting the homepage

--- front-end/library-system-client/src/app/app.module.ts
+++ front-end/library-system-client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { NavComponent } from './components/homepage/nav/nav.component';
+import { SearchComponent } from './components/homepage/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
