4e8974f468f9e0737af0fca4e7b2881f41b0b9f3
Georgi Krastev <gkrastev9715@abv.bg>
2019-10-25 18:41:14 +0300

    for testing purposes

--- front-end/library-system-client/src/app/app.module.ts
+++ front-end/library-system-client/src/app/app.module.ts
@@ -4,23 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { NavComponent } from './components/homepage/nav/nav.component';
-import { SearchComponent } from './components/homepage/search/search.component';
 import { MainPageBookComponent } from './components/homepage/main-page-book/main-page-book.component';
-import { FooterComponent } from './components/homepage/footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { NavComponent } from './components/nav/nav.component';
+import { SearchComponent } from './components/nav/search/search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    NavComponent,
-    SearchComponent,
     MainPageBookComponent,
     FooterComponent,
     LoginComponent,
     RegisterComponent,
+    NavComponent,
+    SearchComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
