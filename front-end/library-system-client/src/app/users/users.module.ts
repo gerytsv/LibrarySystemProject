68d860935307ce918a8276ef21bc0b99e5195e32
Georgi Krastev <gkrastev9715@abv.bg>
2019-10-30 14:23:21 +0200

    adding users modules

---
+++ front-end/library-system-client/src/app/users/users.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from '../components/login/login.component';
+import { Routes } from '@angular/router';
+import { RegisterComponent } from '../components/register/register.component';
+import { SharedModule } from '../shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersRoutingModule } from './users-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [
+    SharedModule , HttpClientModule, UsersRoutingModule, CommonModule, FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [LoginComponent, RegisterComponent]
+})
+export class UsersModule { }
