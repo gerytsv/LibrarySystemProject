8fd71104f36919d6a3705a4aaa51c76466df3113
Georgi Krastev <gkrastev9715@abv.bg>
2019-10-30 14:22:08 +0200

    functionality added

--- front-end/library-system-client/src/app/components/register/register.component.ts
+++ front-end/library-system-client/src/app/components/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { DialogService } from 'src/app/core/services/dialog.service';
+import { Validator } from 'class-validator';
+import { AuthService } from '../../core/services/auth.service';
+import { NotificatorService } from '../../core/services/notificator.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegistrationValidator } from '../../core/validators/compare-password';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +13,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  private readonly validator = new Validator();
+  public registerForm: FormGroup;
+  public passwordFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(
+    private readonly dialogService: DialogService,
+    private readonly authService: AuthService,
+    private readonly notificator: NotificatorService,
+    private readonly fb: FormBuilder,
+    private readonly router: Router,
+  ) {
+    this.passwordFormGroup = this.fb.group({
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+      repeatPassword: ['', Validators.required]
+    }, {
+      validator: RegistrationValidator.validate.bind(this)
+    });
 
-  ngOnInit() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: this.passwordFormGroup.value.password
+    });
   }
 
+  ngOnInit() {
+
+  }
+
+
+  public register() {
+    console.log({...this.registerForm.value, password: this.passwordFormGroup.value.password});
+    const user = {...this.registerForm.value, password: this.passwordFormGroup.value.password};
+    this.authService.register(user)
+      .subscribe(
+        () => {
+          this.notificator.success(`Registration successful!`);
+
+          // then login
+          this.authService.login({username: user.username, password: user.password})
+          .subscribe(
+            () => {
+              this.notificator.success(`Login successful!`);
+              this.router.navigate(['/home']);
+              this.dialogService.closeAll();
+            },
+            () => this.notificator.error(`Invalid email/password!`),
+          );
+
+        },
+        () => this.notificator.error(`Username is already taken`),
+      );
+  }
+
+  public sentTo() {
+    this.dialogService.sentToLogin();
+  }
 }
