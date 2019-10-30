e76c6b8e974f15a82280ca336017ff1878b16cf7
Georgi Krastev <gkrastev9715@abv.bg>
2019-10-30 14:20:17 +0200

    some changes in the app module

--- front-end/library-system-client/src/app/app.component.ts
+++ front-end/library-system-client/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from './common/users/user';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'library-system-client';
+
+  private loggedInSubscription: Subscription;
+  private userSubscription: Subscription;
+
+  constructor(
+    private readonly authService: AuthService,
+  ) {}
+
+  public loggedIn: boolean;
+  public user: User;
+
+  public ngOnInit() {
+    this.loggedInSubscription = this.authService.isLoggedIn$.subscribe(
+      loggedIn => this.loggedIn = loggedIn,
+    );
+    this.userSubscription = this.authService.loggedUser$.subscribe(
+      user => this.user = user,
+    );
+  }
+
 }
--- front-end/library-system-client/src/app/app.module.ts
+++ front-end/library-system-client/src/app/app.module.ts
@@ -1,37 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { MainPageBookComponent } from './components/homepage/main-page-book/main-page-book.component';
-import { FooterComponent } from './components/footer/footer.component';
+
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { NavComponent } from './components/nav/nav.component';
-import { SearchComponent } from './components/nav/search/search.component';
+
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { AllBooksPreviewComponent } from './components/books/all-books-preview/all-books-preview.component';
 import { SingleBookComponent } from './components/books/single-book/single-book.component';
+import { SharedModule } from './shared/shared.module';
+import { UsersModule } from './users/users.module';
+import { CoreModule } from './core/core.module';
+import { ToastrModule } from 'ngx-toastr';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageBookComponent,
-    FooterComponent,
-    LoginComponent,
-    RegisterComponent,
-    NavComponent,
-    SearchComponent,
-    LoginComponent,
-    RegisterComponent,
     HomepageComponent,
     AllBooksPreviewComponent,
-    SingleBookComponent
+    SingleBookComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, CommonModule, HttpClientModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    UsersModule,
+    CoreModule, ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      countDuplicates: true,
+    }),
+    MaterialModule
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [LoginComponent, RegisterComponent]
 })
 export class AppModule {}
