44f7180c2ea231d55e8e676dff9fde8309598f4c
Georgi Krastev <gkrastev9715@abv.bg>
2019-10-30 14:21:57 +0200

    functionality added

--- front-end/library-system-client/src/app/components/login/login.component.ts
+++ front-end/library-system-client/src/app/components/login/login.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { DialogService } from 'src/app/core/services/dialog.service';
+import { Validator } from 'class-validator';
+import { AuthService } from '../../core/services/auth.service';
+import { NotificatorService } from '../../core/services/notificator.service';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { UserLoginDTO } from '../../common/users/user-login-dto';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +15,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+
+  private readonly validator = new Validator();
+
+  constructor(
+    private readonly dialogService: DialogService,
+    private readonly authService: AuthService,
+    private readonly notificator: NotificatorService,
+    private readonly router: Router,
+    private readonly fb: FormBuilder,
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+    });
+   }
 
   ngOnInit() {
+  }
+
+  public login() {
+    console.log(this.loginForm.value);
+    this.authService.login(
+      this.loginForm.value
+    )
+    .subscribe(
+      () => {
+        this.notificator.success(`Login successful!`);
+        this.dialogService.closeAll();
+        this.router.navigate(['/home']);
+        this.dialogService.closeAll();
+      },
+      () => this.notificator.error(`Invalid email/password!`),
+    );
+
+  }
+
+  public sentTo() {
+    this.dialogService.sentToRegister();
   }
 
 }
