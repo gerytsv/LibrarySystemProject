1ba5f4685143351b522047b8104b136d3d9f2b45
Georgi Krastev <gkrastev9715@abv.bg>
2019-10-30 14:20:00 +0200

    adding routes

--- front-end/library-system-client/src/app/app-routing.module.ts
+++ front-end/library-system-client/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: 'home', component: HomepageComponent },
+
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule)},
+
 
   // { path: 'not-found', component: NotFoundComponent },
 
