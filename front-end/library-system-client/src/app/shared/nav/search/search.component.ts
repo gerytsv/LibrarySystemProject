5f1d60836216b839da703e663eeb5559064bc731
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-04 13:53:14 +0200

    search functionality added

--- front-end/library-system-client/src/app/shared/nav/search/search.component.ts
+++ front-end/library-system-client/src/app/shared/nav/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { BooksDataService } from '../../../components/books/books-data.service';
+import { SearchService } from '../../../core/services/search.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  public query = 'title';
+  public input: string;
 
-  constructor() { }
+  ngOnInit() {}
+  constructor(
+    private readonly searchService: SearchService) {}
 
-  ngOnInit() {
+  public sendSearchInfo() {
+    this.searchService.collectSearchInfo(this.query, this.input);
   }
 
 }
