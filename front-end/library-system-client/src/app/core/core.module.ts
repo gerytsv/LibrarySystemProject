7872725075a3821dc5fa48fde4ebf8aecfa67e4c
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-04 13:51:54 +0200

    added resolvers

--- front-end/library-system-client/src/app/core/core.module.ts
+++ front-end/library-system-client/src/app/core/core.module.ts
@@ -1,23 +1,30 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { DialogService } from './services/dialog.service';
-import { SharedModule } from '../shared/shared.module';
 import { NotificatorService } from './services/notificator.service';
 import { StorageService } from './services/storage.service';
 import { AuthService } from './services/auth.service';
-import { ToastrModule } from 'ngx-toastr';
 import { AllBooksResolver } from './resolvers/all-books.service';
 import { BorrowedBooksResolver } from './resolvers/borrowed-books.service';
+import { SearchService } from './services/search.service';
+import { RatedBooksResolver } from './resolvers/rated-books.service';
+import { ReviewedBooksResolver } from './resolvers/reviewed-books.service';
+
 
 
 
 @NgModule({
   declarations: [],
-  imports: [
-     SharedModule
+  imports: [],
+  providers: [
+    DialogService,
+    AuthService,
+    NotificatorService,
+    StorageService,
+    AllBooksResolver,
+    BorrowedBooksResolver,
+    RatedBooksResolver,
+    ReviewedBooksResolver
   ],
-  providers: [DialogService, AuthService, NotificatorService, StorageService, AllBooksResolver, BorrowedBooksResolver],
-  exports: [
-  ]
+  exports: []
 })
-export class CoreModule { }
+export class CoreModule {}
