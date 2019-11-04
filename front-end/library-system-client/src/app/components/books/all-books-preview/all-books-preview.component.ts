b19851eff7cb783e18111f9733e224e6af5bb726
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-04 13:51:30 +0200

    on init logic changed

--- front-end/library-system-client/src/app/components/books/all-books-preview/all-books-preview.component.ts
+++ front-end/library-system-client/src/app/components/books/all-books-preview/all-books-preview.component.ts
@@ -1,18 +1,71 @@
 import { BooksDataService } from './../books-data.service';
 import { BookDTO } from '../models/book.dto';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ChangeDetectorRef,
+  AfterContentInit,
+  OnDestroy
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { SearchService } from '../../../core/services/search.service';
 
 @Component({
   selector: 'app-all-books-preview',
   templateUrl: './all-books-preview.component.html',
   styleUrls: ['./all-books-preview.component.css']
 })
-export class AllBooksPreviewComponent implements OnInit {
+export class AllBooksPreviewComponent implements OnInit, OnDestroy {
   public books: BookDTO[] = [];
-  constructor(private route: ActivatedRoute) {}
+  public title;
+
+  private searchInfoInSubscription: Subscription;
+  private routeSubscription: Subscription;
+
+  constructor(
+    private route: ActivatedRoute,
+    private searchInfo: SearchService,
+    private readonly booksServive: BooksDataService
+  ) {}
 
   ngOnInit() {
-   this.route.data.subscribe(({books}) => this.books = books);
+    // tslint:disable-next-line: no-string-literal
+    if (this.route.snapshot['_routerState'].url !== '/books/search') {
+      this.routeSubscription = this.route.data.subscribe(({ books }) => {
+        books.books.subscribe(booksArray => {
+          this.title = `My ${books.title}`;
+          this.books = booksArray;
+          if (this.books.length === 0) {
+            this.title = `No ${books.title} at the moment`;
+          }
+        });
+      });
+    } else {
+      this.searchInfoInSubscription = this.searchInfo.SearchInfo$.subscribe(
+        object => {
+          this.booksServive
+            .searchBooks(object.query, object.input)
+            .subscribe(books => {
+             this.books = books ;
+             if (this.books.length === 0) {
+               this.title = `No results found for books with ${object.query} ${object.input}`;
+             } else {
+               this.title = `Showing results for books with ${object.query} ${object.input}`;
+             }
+            });
+        }
+      );
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+    if (this.searchInfoInSubscription) {
+      this.searchInfoInSubscription.unsubscribe();
+    }
   }
 }
